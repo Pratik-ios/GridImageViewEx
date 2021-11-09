03154be268c0fbebf550c87993db60faa918e012
Pratik-Appic <93186565+PratikPrk-Appic@users.noreply.github.com>
2021-11-09 17:02:09 +0530

    Grouped the files

--- GrideImageViewEx/Screens/GridImageView/GridImageViewViewController.swift
+++ GrideImageViewEx/Screens/GridImageView/GridImageViewViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GridImageViewViewController.swift
 //  GrideImageViewEx
 //
 //  Created by Pratik on 09/11/21.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GridImageViewViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
 
 }
-
